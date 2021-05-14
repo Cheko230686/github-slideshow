901a003dedff1b9e5db4eda89e44eb41059d7739
Cheko230686 <84147889+Cheko230686@users.noreply.github.com>
2021-05-14 18:20:11 -0500

    Add files via upload

---
+++ build/js/bundle.js
@@ -0,0 +1,7 @@
+function cambiarClase() {
+    const siteNav = document.getElementById('navegacion');
+    siteNav.classList.toggle('site-nav-open');
+    const menuOpen = document.getElementById('menu-toggle');
+    menuOpen.classList.toggle('menu-open');
+
+}
